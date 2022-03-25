eae8c1af04ae297f73a23854bc51743c2ec885df
Prismly <prismly55@gmail.com>
2022-03-25 16:08:59 -0400

    Camera path following
    The main camera will now follow a path defined by a dedicated game object and its children, which will allow tracks to curve and do more than just be straight

--- GreenTheftAuto/Assets/Scripts/CameraController.cs
+++ GreenTheftAuto/Assets/Scripts/CameraController.cs
@@ -4,11 +4,57 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] [Tooltip("")] private GameObject playerObject;
+    [SerializeField] [Tooltip("")] private Transform playerTransform;
+    [SerializeField] [Tooltip("")] private Transform pathTransform;
+    [SerializeField] [Tooltip("")] private float followSpeed;
+    [SerializeField] [Tooltip("")] private float turnSpeed;
+    private Transform targetPathPoint;
     [SerializeField] [Tooltip("")] private Vector3 offset;
+    private bool followingPath = true;
+    private bool turning = false;
+    private int pathIndex;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startOfSeg;
+        Vector3 endOfSeg;
+        Color[] rainbow = { Color.red, new Color(1, 0.5f, 0), Color.yellow, Color.green, Color.blue, new Color(0.5f, 0, 1), Color.magenta }; 
+        for (int i = 0; i < pathTransform.childCount - 1; i++)
+        {
+            startOfSeg = pathTransform.GetChild(i).position;
+            endOfSeg = pathTransform.GetChild(i + 1).position;
+            Gizmos.color = rainbow[i % rainbow.Length];
+            Gizmos.DrawLine(startOfSeg, endOfSeg);
+        }
+    }
+
+    private void Start()
+    {
+        pathIndex = 0;
+        targetPathPoint = pathTransform.GetChild(0);
+        turning = true;
+    }
 
     private void Update()
     {
-        transform.position = playerObject.transform.position + offset;
+        if (Vector3.Distance(transform.position, targetPathPoint.position) < 0.1f)
+        {
+            // The camera has finished traveling along the current path segment
+            if (pathIndex < pathTransform.childCount - 1)
+            {
+                // There is at least one more segment for the camera to follow; start following it
+                pathIndex++;
+                targetPathPoint = pathTransform.GetChild(pathIndex);
+            }
+            else
+            {
+                followingPath = false;
+            }
+        }
+
+        if (followingPath)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPathPoint.position, followSpeed * Time.deltaTime);
+        }
     }
 }
